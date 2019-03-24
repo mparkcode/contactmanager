3198534f3e3fd97cd58d19cb437074f1c323f910
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2019-03-24 18:35:46 +0000

    Updates

---
+++ precache-manifest.23d5e91469bd72b3cb79e10cbd05c3ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "e3d56f415d1e2611ff8e",
+    "url": "/contactmanager/static/js/main.b8dc8736.chunk.js"
+  },
+  {
+    "revision": "1eb863042967fbe2fd17",
+    "url": "/contactmanager/static/js/2.8ca13d7e.chunk.js"
+  },
+  {
+    "revision": "e3d56f415d1e2611ff8e",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1eb863042967fbe2fd17",
+    "url": "/contactmanager/static/css/2.d7cb3695.chunk.css"
+  },
+  {
+    "revision": "fcd5aace263513e8851d9f6e74a46475",
+    "url": "/contactmanager/index.html"
+  }
+];
